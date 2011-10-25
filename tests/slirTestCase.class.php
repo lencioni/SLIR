19665532d025c33e6157586127acc58b381cae97
Joe Lencioni <joe.lencioni@gmail.com>
2011-10-25 11:30:34 -0500

    Set up the instance of SLIR as a property of the object so it can be used by test cases if we want to.

--- tests/slirTestCase.class.php
+++ tests/slirTestCase.class.php
@@ -3,14 +3,30 @@ require_once 'core/slir.class.php';
 
 abstract class SLIRTestCase extends PHPUnit_Framework_TestCase
 {
+  /**
+   * @var SLIR
+   */
+  protected $slir;
+
+  /**
+   * @return void
+   */
   protected function setUp()
   {
-    $slir = new SLIR();
-    $slir->getConfig();
+    $this->slir = new SLIR();
+    $this->slir->getConfig();
     SLIRConfig::$defaultImagePath = null;
     SLIRConfig::$forceQueryString = false;
 
     // Try to fix documentRoot for CLI
     SLIRConfig::$documentRoot = preg_replace('`/slir/?$`', '', SLIRConfig::$documentRoot);
+  }
+
+  /**
+   * @return void
+   */
+  protected function tearDown()
+  {
+    unset($this->slir);
   }
 }
